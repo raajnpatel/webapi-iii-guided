15fb4177b9821308486fef2ee8d55c90881db7ea
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-04 10:31:26 -0800

    gatekeeper

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next();
+}
+
+function gateKeeper(req, res, next){
+  if(req.headers.password !== 'mellon'){
+    res.status(401)
+  } else {
+    next()
+  }
+}
+
+
+server.use(helmet()); //
 server.use(express.json());
+server.use(logger);
+server.use(gateKeeper);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get("/echo", (req, res) => {
+  res.send(req.headers);
+});
+
+// Used Locally
+server.get("/area51", helmet(), (req, res) => {
+  res.send(req.headers);
 });
 
 module.exports = server;
